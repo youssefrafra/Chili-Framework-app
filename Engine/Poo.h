a2ca2012446f69da295ffb37eafa09402ad08eb2
Youssef <rafrafi.youssef@hotmail.com>
2022-01-28 13:31:05 +0100

    Add project files.

---
+++ Engine/Poo.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Graphics.h"
+#include "Dude.h"
+
+class Poo
+{
+public:
+	Poo(int in_x, int in_y, int in_vx, int in_vy);
+	void Update();
+	void ProcessConsumption( const Dude& dude );
+	void Draw( Graphics& gfx ) const;
+	bool IsEaten() const;
+private:
+	int x;
+	int y;
+	int vx;
+	int vy;
+	static constexpr int width = 24;
+	static constexpr int height = 24;
+	bool isEaten = false;
+};
